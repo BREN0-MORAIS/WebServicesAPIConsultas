e0aee5788f31d1495dc9b31d1d880666128aebd9
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-11-29 20:38:30 -0400

    Foi implementados os Models e Instalado as Dependencias necessarias para rodar a API.

---
+++ WebServicesAPIConsultas/WebServicesAPIConsultas/Models/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServicesAPIConsultas.Models
+{
+    public class Medico
+    {
+            [Key]
+            [Display(Name = "Código")]
+            public int MedicoId { get; set; }
+
+            public string Nome { get; set; }
+            public string Especialidade { get; set; }
+    }
+}
---
+++ WebServicesAPIConsultas/WebServicesAPIConsultas/Models/Paciente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServicesAPIConsultas.Models
+{
+    public class Paciente
+    {
+        [Key]
+        [Display(Name = "Código")]
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        [Display(Name = "Data Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Medico")]
+        public int MedicoId { get; set; }
+        [Display(Name = "Hora Consulta")]
+        public DateTime HoraConsulta { get; set; }
+        [Display(Name = "Data Consulta")]
+        public DateTime DataConsulta { get; set; }
+
+    }
+}
